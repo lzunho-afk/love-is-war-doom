37de0c4d17b4ab2a41ccbaa6486d65994e2342e7
Lucas Zunho <lucaszunho17@gmail.com>
2023-08-16 20:42:31 -0300

    Arquivo requirements & batch de desenvolvimento.

---
+++ make.bat
@@ -0,0 +1,50 @@
+@ECHO OFF
+
+rem Dev. Commandline - Loveiswar Engine (LIWE)
+type LICENSE
+echo:
+
+rem Ajustando o virtualenv do python conforme necessário
+if EXIST .venv\Scripts\activate.bat (
+    .venv\Scripts\activate.bat
+) else (
+    GOTO setupenv
+)
+
+rem Variáveis de uso global
+set DOCS_PATH=".\docs"
+set DOCS_BATCH="%DOCS_PATH%\make.bat"
+
+rem Realizando o parsing dos argumentos da linha de comando
+IF "%1" == "" GOTO help
+IF "%1" == "documentation" GOTO bdocs
+
+:bdocs
+rem Compilação da documentação com Sphinx
+pushd .\docs
+sphinx-apidoc -o . .. ..\setup.py
+make.bat html
+popd
+GOTO end
+
+:setupenv
+rem Configuração do venv para o ambiente
+python -m pip install -U venv
+python -m venv .venv
+.venv\Scripts\activate.bat
+python -m pip install -U pip
+python -m pip install -r requirements.txt
+GOTO end
+
+:help
+echo PAINEL DE AJUDA:
+echo    * documentation     - Compila a documentacao com sphinx.
+echo    * setupenv          - Roda os comandos de definição do venv da engine.
+echo    * package           - Realiza o empacotamento do projeto (nao funciona).
+echo    * clean             - Realiza a limpeza de todas as operacoes caso nenhuma
+echo                           seja especificada individualmente.
+echo                            \- * documentation; * requirements; * package.
+echo                            \- ex.: .\make.bat clean documentation
+echo:
+
+:end
