b6c6d0cc83932efc264b3e8687993bb4e7e861f6
Lucas Zunho <lucaszunho17@gmail.com>
2023-08-30 20:41:00 -0300

    Adicionado arquivo de topo para execução do pacote

---
+++ loveiswar.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright (c) MIT
+# Lucas Zunho <lucaszunho17@gmail.com>
+
+from loveiswar import main
+    
+if __name__ == "__main__":
+    game = Game()
+    game.run()
--- loveiswar/main.py
+++ loveiswar/main.py
@@ -115,7 +115,3 @@ class Game:
             self.check_events()
             self.update()
             self.draw()
-
-if __name__ == "__main__":
-    game = Game()
-    game.run()
